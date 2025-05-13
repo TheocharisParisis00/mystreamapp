9a37bf0f959a6b50045b51a8be9c963e22e83cb8
θεοχαρης παρισης <parisisxaris@gmail.com>
2025-05-13 15:07:07 +0200

    .

--- public/create_playlist.php
+++ public/create_playlist.php
@@ -30,20 +30,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/theme.css">
+    <link rel="stylesheet" href="assets/css/create_playlist.css">
+
     <title>Create New Playlist</title>
-</head>  
+</head> 
 <body>
-<a href="userpage.php"><button>Go back</button></a>
-<form method="POST" action="">
-    <input type="hidden" name="action" value="create_playlist">
+  <div class="top-right">
+    <a href="userpage.php"><button>Go back</button></a>
+  </div>
+  <div class="form-container">
+    <form method="POST" action="">
+      <input type="hidden" name="action" value="create_playlist">
 
-    <label for="playlist_name">Playlist Name:</label><br>
-    <input type="text" id="playlist_name" name="playlist_name" required><br><br>
+      <label for="playlist_name">Playlist Name:</label><br>
+      <input type="text" id="playlist_name" name="playlist_name" required><br><br>
 
-    <label for="description">Description:</label><br>
-    <textarea id="description" name="description" rows="4" cols="50"></textarea><br><br>
+      <label for="description">Description:</label><br>
+      <textarea id="description" name="description" rows="4" cols="50"></textarea><br><br>
 
-    <input type="submit" value="Create Playlist">
-</form>
+      <input type="submit" value="Create Playlist">
+    </form>
+  </div>
+  <script src="assets/js/main.js"></script>
 </body>
-</html> 
+</html>
